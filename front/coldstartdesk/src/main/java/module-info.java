85d90b1a0a2be72fb2f6952fe10dd264c7caa2d5
Renato Fraga <renato.fraga93@gmail.com>
2026-06-19 00:06:18 -0300

    commit CI/CD

--- front/coldstartdesk/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.coldstartdesk {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.coldstartdesk to javafx.fxml;
-    exports com.example.coldstartdesk;
-}
